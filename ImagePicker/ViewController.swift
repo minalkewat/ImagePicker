122a9afd1cfc215242afc91a68151ff9b9b8437b
meenal kewat <happykraftsinnovations@gmail.com>
2019-03-01 13:07:16 +0530

    Import An Image From Photo Library In XCode 9 (Swift 4.0)

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imagePicker = UIImagePickerController()
 
+    @IBOutlet weak var img: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
     }
 
 
+    @IBAction func onPickAction(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+}
+
+
+// Wring Extention increase the readablity of code
+extension ViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+            img.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
 
